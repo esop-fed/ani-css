eab3d4ef4dfce065e16ecf213d2e2a981cd0df21
niannings <niannings@163.com>
2019-09-08 18:58:22 +0800

    fate(book): wlxm book updated

--- components/card-list.js
+++ components/card-list.js
@@ -2,7 +2,7 @@ import { createFragmentByTemplateString } from '../utils/dom.js';
 
 export const createCard = ({ title, path, description }) => {
     const templateString = `
-        <a class="card flow-wave" href="${path}">
+        <a class="card card-3D flow-wave" href="${path}">
             <h3 class="card__title">${title}</h3>
             <p class="card__description">${description}</p>
         </a>
---
+++ programs/book/wlxm/index.js
@@ -0,0 +1,32 @@
+const book = document.querySelector('.book');
+const data = 'QW,ER,TY,UI,OP,AS,DF,GH,JK,LZ,XC,VB,NM'.split(',');
+const len = data.length;
+
+book.innerHTML = data.reduce((html, [a, b]) => {
+    html += `
+        <div class="page card">
+            <div class="page-front">${a}</div>
+            <div class="page-back">${b}</div>
+        </div>
+    `;
+
+    return html;
+}, ``);
+
+const pages = book.querySelectorAll('.page')
+
+pages.forEach((node, index) => {
+    node.style.zIndex = len - index;
+    node.style.top = index + 'px';
+    node.style.right = -index + 'px';
+
+    node.addEventListener('click', e => {
+        node.classList.toggle('page--active');
+
+        if (node.classList.contains('page--active')) {
+            node.style.zIndex = index;
+        } else {
+            node.style.zIndex = len - index;
+        }
+    })
+})
