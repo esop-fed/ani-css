47f8d82052f92945904b4c4d3ddce88917005701
niannings <niannings@163.com>
2019-09-19 09:29:25 +0800

    particles

--- webAssembly/particle/index.js
+++ webAssembly/particle/index.js
@@ -1,3 +1,48 @@
+class Particle {
+    // 重力加速度
+    acceleration = 0.25;
+    // 弹性碰撞 速率衰减
+    elasticCollision = 0.99;
+
+    constructor({
+        edge = { x: 0, y: 0 },
+        speed = { x: 1, y: 1 },
+        color = '#e98484',
+        r = 4,
+    }) {
+        const { random } = Math;
+        const [rx, ry, rz] = [random(), random(), random()];
+
+        this.x = rx * edge.x;
+        this.y = ry * edge.y;
+        this.vx = rz > 0.5 ? rx * speed.x : -rx * speed.x;
+        this.vy = rz > 0.5 ? ry * speed.y : -ry * speed.y;
+        this.color = color;
+        this.r = r;
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+         /**
+         * arc(x, y, radius, startAngle, endAngle, anticlockwise)
+         */
+        ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+
+    move(edge) {
+        this.x += this.vx;
+        this.y += this.vy;
+
+        // this.vy *= this.elasticCollision
+        // this.vy += this.acceleration
+
+        this.vx = this.x < edge.x && this.x > 0 ? this.vx : -this.vx;
+        this.vy = this.y < edge.y && this.y > 0 ? this.vy : -this.vy;
+    }
+}
+
 const app = document.getElementById('app');
 
 function initial(app) {
@@ -12,91 +57,67 @@ function initial(app) {
     app.appendChild(canvas);
 
     const ctx = canvas.getContext('2d');
-    const draw = initialDraw(ctx, edge);
+    const draw = initialDraw(ctx, edge, canvas);
 
-    requestAnimationFrame(draw);
+    draw();
 }
 
 initial(app);
 
-function initialDraw(ctx, edge) {
-    const drawParticle = initialDrawParticle({ total: 100, edge });
-
-    return function draw() {
-        ctx.clearRect(0, 0, edge.x, edge.y);
-        const particles = drawParticle(ctx);
-
-        drawLine(ctx, particles);
-    };
-}
-
-function initialDrawParticle({
-    total = 10,
-    r = 8,
-    color = '#f00',
-    edge = { x: 0, y: 0 }
-}) {
+function initialDraw(ctx, edge, canvas) {
     const particles = [];
-    const { random } = Math;
+    const total = 100;
+    const mouseParticle = new Particle({ edge, color: '#f0f', r: 2, speed: { x: 0, y: 0} });
 
-    for (let i = 0; i < total; i++) {
-        particles.push({
-            x: random() * edge.x,
-            y: random() * edge.y
-        });
+    for (let i = 0; i < total - 1; i++) {
+        particles.push(new Particle({ edge }));
     }
 
-    return function drawParticle(ctx) {
-        const { PI } = Math;
+    particles.push(mouseParticle);
 
+    canvas.addEventListener('mousemove', e => {
+        const { clientX: x, clientY: y } = e;
+
+        mouseParticle.x = x;
+        mouseParticle.y = y;
+    })
+
+    return function draw() {
+        ctx.fillStyle = 'rgba(255,255,255,0.3)';
+        ctx.fillRect(0, 0, edge.x, edge.y);
+        // ctx.clearRect(0, 0, edge.x, edge.y);
+        
         for (let i = 0; i < total; i++) {
-            const pos = particles[i];
-
-            ctx.fillStyle = color;
-            ctx.beginPath();
-            /**
-             * arc(x, y, radius, startAngle, endAngle, anticlockwise)
-             */
-            ctx.arc(pos.x, pos.y, r, 0, 2 * PI);
-            ctx.fill();
+            particles[i].draw(ctx);
+            particles[i].move(edge);
         }
 
-        return particles;
+        drawLine(ctx, particles);
+
+        requestAnimationFrame(draw);
     };
 }
 
 function drawLine(ctx, particles = []) {
     const len = particles.length;
-    const { sqrt } = Math;
-    let d = 0;
-    let p;
-    let q;
-
-    ctx.strokeStyle = '#f0f';
-
-    console.log(1);
 
     for (let i = 0; i < len; i++) {
         p = particles[i];
 
         for (let j = i + 1; j < len; j++) {
-            q = particles[j];
-            d = sqrt((p.x - q.x) * (p.x - q.x) + (p.y - q.y) * (p.y - q.y));
-
-            if (d <= 200) {
-                ctx.beginPath();
-                ctx.moveTo(p.x, p.y);
-                ctx.lineTo(q.x, q.y);
-                ctx.stroke();
-            }
+            line(ctx, p, particles[j]);
         }
-
-        // move(ctx, { pos: { x: p.x, y: p.y } });
     }
 }
 
-function move(ctx, { pos = { x: 0, y: 0 }, speed = 1 }) {
-    pos.x += speed;
-    pos.y += speed;
-    ctx.translate(pos.x, pos.y);
+function line(ctx, p, q, color = '#d9d9d9', maxDistance = 60) {
+    const d = Math.sqrt((p.x - q.x) * (p.x - q.x) + (p.y - q.y) * (p.y - q.y));
+
+    if (d <= maxDistance) {
+        ctx.strokeStyle = color;
+        ctx.beginPath();
+        ctx.moveTo(p.x, p.y);
+        ctx.lineTo(q.x, q.y);
+        ctx.stroke();
+    }
 }
