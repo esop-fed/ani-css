56fb3d7b1e9940b4d5ca4ed24b0c852f6074a100
shujianbo <shujianbo@itiger.com>
2020-03-06 16:47:40 +0800

    注册sw

--- src/config.js
+++ src/config.js
@@ -1,22 +1,22 @@
 // register service worker
 
-// if ('serviceWorker' in navigator) {
-//     navigator.serviceWorker
-//         .register('/ani-css/sw.js', { scope: '/ani-css/' })
-//         .then(function(reg) {
-//             if (reg.installing) {
-//                 console.log('Service worker installing');
-//             } else if (reg.waiting) {
-//                 console.log('Service worker installed');
-//             } else if (reg.active) {
-//                 console.log('Service worker active');
-//             }
-//         })
-//         .catch(function(error) {
-//             // registration failed
-//             console.log('Registration failed with ' + error);
-//         });
-// }
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/ani-css/sw.js', { scope: '/ani-css/' })
+        .then(function(reg) {
+            if (reg.installing) {
+                console.log('Service worker installing');
+            } else if (reg.waiting) {
+                console.log('Service worker installed');
+            } else if (reg.active) {
+                console.log('Service worker active');
+            }
+        })
+        .catch(function(error) {
+            // registration failed
+            console.log('Registration failed with ' + error);
+        });
+}
 
 export default {
     project: [
